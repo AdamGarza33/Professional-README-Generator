46a0dd99664b2f2eefa6b92441e9613d616e0226
Addit <adam.garza33@hotmail.com>
2023-03-29 02:34:20 -0500

    function that creates license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,13 +11,31 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  let licenselink;
+  let licenseLink;
   // Select the correct license link
+  switch(license) {
+    case 'MIT':
+      licenseLink = 'https://mit-license.org/';
+      break;
+      case 'Microsoft Public License':
+        licenseLink = 'https://opensource.org/license/ms-pl-html/';
+        break;
+        case 'ISC':
+          licenseLink = 'https://opensource.org/license/isc-license-txt/';
+          break;
+        default:
+          licenseLink = '';
+          break;
+  }
+  return licenseLink
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = '';
+  // If license is selected creates license section
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
