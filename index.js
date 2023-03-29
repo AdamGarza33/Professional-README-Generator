872175caefeb5f9617216ee5dceefbb543ce1ead
Addit <adam.garza33@hotmail.com>
2023-03-29 00:26:37 -0500

    included inquirer and fs for the app as well as created an array for quesitons

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your project title?',
+    'What is the description to your project?',
+    'What are the installation intstructions?',
+    'What is the usage information?',
+    'What are the contribution guidelines?',
+    'What are the test instructions?',
+    'What license is your project using?',
+    'What is your GitHub username?',
+    'What is your GitHub email?'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
