d900d5b347eff3d72c8eda780f9a5ba394a2e7f3
Addit <adam.garza33@hotmail.com>
2023-03-29 00:41:16 -0500

    added first question to the array

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
-    'What is your project title?',
-    'What is the description to your project?',
-    'What are the installation intstructions?',
-    'What is the usage information?',
-    'What are the contribution guidelines?',
-    'What are the test instructions?',
-    'What license is your project using?',
-    'What is your GitHub username?',
-    'What is your GitHub email?'
+    // Projects Name
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title? (Required)',
+        validate: titleInput => {
+            if(titleInput) {
+                return true;
+            } else {
+                console.log('A title is required to continue.');
+                return false;
+            }
+        }
+    },
+
+    
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
 init();
